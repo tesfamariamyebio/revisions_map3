220bda842fb0759ccf7f26da9dea720ea2d6d7a3
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-08-19 13:21:56 +0200

    'revisionmap3'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './componnent/Card';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      students:[]
+    }
+  }
+  render(){
+const students=[{
+  id: 'abc',
+  name: 'Evelyn',
+  surname: 'Arnett'
+  }, {
+  id: 'def',
+  name: 'Glenn',
+  surname: 'Bradford'
+  }, {
+  id: 'ghi',
+  name: 'Kourtney',
+  surname: 'Calloway'
+  }, {
+  id: 'jkl',
+  name: 'Dawson',
+  surname: 'Eagerton'
+  }, {
+  id: 'mno',
+  name: 'Maggie',
+  surname: 'Townsend'
+  }, {
+  id: 'pqr',
+  name: 'Kyle',
+  surname: 'Millhouse'
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+];
+    return (
+      <div className="container">
+        <div className="row">
+        {students.map((x)=>{
+          
+          return(
+            
+                <div className="col-3">
+                 
+                            <ul className="list-group text-center font-weight-normal" >
+                               <li className="list-group-item"> {x.name} {x.surname}
+                               </li>
+                            </ul>
+                </div>            
+          )
+        })}
+        </div>
+        okk 
     </div>
-  );
+    );
+  }
 }
-
 export default App;
---
+++ src/componnent/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Card extends React.Component{
+    render(){
+        return(
+            <div>
+                card 
+            </div>
+        );
+            
+        
+}
+}
+export default Card ;
